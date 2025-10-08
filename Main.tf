d2ff9abe3faec693af3178b246749ec3632c5324
prince nagar <zprinag@gmail.com>
2025-10-08 11:34:32 +0530

    new vn added

--- Main.tf
+++ Main.tf
@@ -54,6 +54,43 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 }
 
+resource "azurerm_network_interface" "nic2" {
+  name                = "my-nic-2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "my-ip-2"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm2" {
+  name                            = "my-vm-2"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  size                            = "Standard_F2"
+  admin_username                  = "adminuser2"
+  admin_password                  = "prince123@!"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.nic2.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
 # Bastion Subnet (must be named "AzureBastionSubnet")
 resource "azurerm_subnet" "bastion" {
   name                 = "AzureBastionSubnet"
